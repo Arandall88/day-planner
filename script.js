906902d59add0257bc62241d969638813deee10e
Angel Randall <drivenbysuccess21@gmail.com>
2020-03-12 20:56:37 -0500

    added times to index.html

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-$("#hour")
+var now = moment(); // new Date().getTime();
+var then = moment().add(39, 'seconds'); // new Date(now + 60 * 1000);
+
+$(".now").text(moment(now).format('h:mm:ss a'));
+$(".then").text(moment(then).format('h:mm:ss a'));
+$(".duration").text(moment(now).to(then));
+(function timerLoop() {
+  
+  $(".difference").text(moment().to(then));
+  $(".countdown").text(countdown(then).toString());
+  $(".hacked").animate("pwned");
+  requestAnimationFrame(timerLoop);
+})();
