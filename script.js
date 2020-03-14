bfef3534733237d57580ac9d9f8e6bce7f34b66e
Angel Randall <drivenbysuccess21@gmail.com>
2020-03-13 21:18:22 -0500

    deleted extra code

--- script.js
+++ script.js
@@ -3,17 +3,4 @@ var tempDate = moment();
 
 tempDate.hour(9) // sets the hour to 9 am
 tempDate.hour(17) // sets hour to 5pm
-
-
-
-// $(".now").text(moment(now).format('h:mm:ss a'));
-// $(".then").text(moment(then).format('h:mm:ss a'));
-// $(".duration").text(moment(now).to(then));
-// (function timerLoop() {
-  
-//   $(".difference").text(moment().to(then));
-//   $(".countdown").text(countdown(then).toString());
-//   $(".hacked").animate("pwned");
-//   requestAnimationFrame(timerLoop);
-// })();
 var dateToday = now.format("dddd, MMMM Do")
